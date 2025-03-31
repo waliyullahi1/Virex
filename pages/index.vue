c0136d16c071a3c60142410f459424eac63d6fde
unknown <waliuwaheed2021@gmail.com>
2025-03-31 15:58:31 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -117,10 +117,10 @@
 
         <section id="contact-us" class=" dark:text-black  px-4">
             <div class="container rounded-md flex flex-col bg-secondary  items-center mx-auto py-10">
-                <TypographyH2> From the <span class=" bg-preprimary text-white px-2 rounded-md">cloud</span> to Customers,
+                <TypographyH2> From the <span class=" bg-preprimary text-white px-2 rounded-md">cloud fdgf</span> to Customers,
                     We Bring all the Answers</TypographyH2>
                 <ButtonsPrimary @click="$router.push('/register')" class="my-5">Get start</ButtonsPrimary>
-                <TypographyP class=" tracking-wide"> Contact Us: +23408166988715</TypographyP>
+                <TypographyP class=" tracking-wide"> Contact Us: dddfdfs +23408166988715</TypographyP>
             </div>
 
         </section>
