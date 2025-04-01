6b0457f242d086e3d5b322f4537f60fe81c3f766
unknown <waliuwaheed2021@gmail.com>
2025-04-01 18:29:27 +0100

    update

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,11 @@ export default defineAppConfig({
       notifications: {
         position: "top-0 bottom-[unset]", // Move notifications to the top right
       },
+      head: {
+        link: [
+          { rel: 'icon', type: 'image/png', href: '/logo.png' }
+        ]
+      }
     },
   });
   
